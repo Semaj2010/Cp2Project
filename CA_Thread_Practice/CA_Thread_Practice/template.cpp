1317208320de2244052691c02cdb3859dd8c1751
unknown <semaj9310@gmail.com>
2016-12-01 09:48:42 +0900

    STL test

--- CA_Thread_Practice/CA_Thread_Practice/template.cpp
+++ CA_Thread_Practice/CA_Thread_Practice/template.cpp
@@ -3,8 +3,88 @@
 #include <vector>
 #include "Frame.h"
 #include "ExArray.h"
+#include <list>
+#include <map>
 
+using namespace std;
+
+void dump(list <string> &l) {
+	list<string>::iterator iter = l.begin();
+	while (iter != l.end()) {
+		cout << *iter << endl;
+		iter++;
+	}
+}
+class Word {
+private:
+	string word;
+public:
+	Word(string word) {
+		this->word = word;
+	}
+	string getWord() {
+		return this->word;
+	}
+};
 void main() {
+	map<char, list<Word>> m;
+
+	list<Word> aList;
+	list<Word> bList;
+
+	aList.push_back(Word("apple"));
+	aList.push_back(Word("avoid"));
+	aList.push_back(Word("appear"));
+
+	bList.push_back(Word("bread"));
+	bList.push_back(Word("bring"));
+	bList.push_back(Word("bow"));
+	m['a'] = aList;
+	m['b'] = bList;
+
+	map<char, list<Word>>::const_iterator iter = m.begin();
+
+	while (iter != m.end()) {
+		cout << iter->first << ":" << endl;
+
+		list<Word> temp = iter->second;
+		list<Word>::const_iterator iter_list = temp.begin();
+
+		while (iter_list != temp.end()) {
+			Word word = *iter_list;
+			cout << word.getWord() << ", ";
+			iter_list++;
+		}
+		cout << "\n===" << endl;
+		iter++;
+	}
+
+
+}
+void main_map() {
+	map<string, int> m;
+	m["Seoul"] = 100;
+	m["daegu"] = 120;
+	m["busan"] = 200;
+
+	cout << "Train to Seoul = " << m["Seoul"] << "$" << endl;
+	cout << "Train to Daegu = " << m["daegu"] << "$" << endl;
+	cout << "Train to Busan = " << m["busan"] << "$" << endl;
+}
+void main_list() {
+
+	list <string> names;
+	names.insert(names.begin(), "Konkuk");
+	names.insert(names.begin(), "University");
+	names.insert(names.begin(), "SCLAB");
+	names.insert(names.begin(), "CSY");
+	dump(names);
+	cout << "===" << endl;
+	names.reverse();
+	dump(names);	
+}
+
+void main2() {
 	/*
 	ExArray<int> arr1(3);
 	ExArray<char> arr2(4);
@@ -58,6 +138,4 @@ void main() {
 		cout << *iter << endl;
 		iter++; // iterators are handles just like pointers
 	}
-
-	return 0;
 }
